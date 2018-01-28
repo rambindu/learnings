2cf1c622d04e792926042753f45c0b9611f2547d
bindu <maturi.bindu.reddy@gmail.com>
2018-01-28 17:05:12 +0530

    to learn programs

--- delarray.c
+++ delarray.c
@@ -1,36 +1,28 @@
 #include<stdio.h>
 int main()
 {
-	int arrname[10],i,loc,j;
-	
+	int arrname[10], i, loc, j;
+
 	printf("Enter the array size : ");
-	scanf("%d",&j);
-	
-		printf("Enter the array element : ");
-		for (i=0 ; i < j ; i++)
-		{
-			scanf("%d", &arrname[i]);
-		}
-			printf("Enter the location to be deleted: ");
-			scanf("%d", &loc);
-		if (loc>=j+1)
-	
+	scanf("%d", &j);
+
+	printf("Enter the array element : ");
+	for (i = 0 ; i < j ; i++)
+		scanf("%d", &arrname[i]);
+
+	printf("Enter the location to be deleted: ");
+	scanf("%d", &loc);
+	if (loc >= j)
 		printf("location can not be deleted : \n");
-		
-		else
-		{
-			for (i=loc-1 ; i < j-1 ;i++ )
-			arrname[i]=arrname[i+1];
-
-		
-			
-			for(i = 0 ; i < j-1 ; i ++)
-			printf("%d \n ", arrname[i]);
+	else{
+		for (i = 0 ; i < j-1; i++ ){
+			if(i >= loc)
+				arrname[i] = arrname[i+1];
 		}
-	
-		//	printf("location can not be deleted : \n");
 
-	
+		for(i = 0 ; i < j-1 ; i++) 
+			printf("%d \n ", arrname[i]);
+	}
 
-return 0;
+	return 0;
 }
